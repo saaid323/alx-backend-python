3dc1a5d021f16f85c95ed0db694bb4fdb0f8c9b4
saaid323 <saaid2538@gmail.com>
2024-01-11 13:33:23 +0000

    added 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 '''return values with the appropriate types'''
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''return values with the appropriate types'''
     return [(i, len(i)) for i in lst]
