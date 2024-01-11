250b6b6fe3d6f3a40b0fb9ea077474a07485f6c1
saaid323 <saaid2538@gmail.com>
2024-01-11 12:49:55 +0000

    added 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''takes two arguments and returns tuple'''
+from typing import Tuple, Mapping, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''function that return tuple'''
+    return (k, v)
