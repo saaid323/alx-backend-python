a64bf77daff59e40894bce7f7365458d26e8db7a
saaid323 <saaid2538@gmail.com>
2024-01-11 10:03:46 +0000

    added 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''takes a float and return dtring representation of the float'''
+
+
+def to_str(n: float) -> str:
+    '''function that takes float argument and return string'''
+    return str(n)
