14ca170c326b564271ba4b0727b8d862697a634a
saaid323 <saaid2538@gmail.com>
2024-01-11 09:53:14 +0000

    added 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''concatinates two string'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function that concatinates two strings
+    """
+    return str1 + str2
