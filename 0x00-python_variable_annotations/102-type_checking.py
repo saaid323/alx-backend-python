22fc9746316e738189873267e7776037446e226b
saaid323 <saaid2538@gmail.com>
2024-01-11 14:17:20 +0000

    added 102-type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from typing import Tuple, List
+'''function that returns a list'''
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Functioon that returns a  list'''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
