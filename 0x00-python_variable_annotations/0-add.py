a1e90e9e43fd5881cfde7e596074f17dedec456a
saaid323 <saaid2538@gmail.com>
2024-01-11 09:47:38 +0000

    added 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''return the sum of wo floats'''
+
+
+def add(a: float, b: float) -> float:
+    ''''parameters :
+           a: float
+           b: float
+        return float that is the sum of a and b
+    '''
+    return a + b
