ae1ed9c2e40063080e4a55c2d0c2e51d4e675c8f
saaid323 <saaid2538@gmail.com>
2024-01-15 15:32:41 +0000

    added 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''return the list of all the delays (float values)'''
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''return the list of all the delays (float values)'''
     lis = []
     for i in range(n):
