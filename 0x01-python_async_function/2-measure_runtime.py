1823b9d8ad8d6127e2f4e24f4f947d5c7f50df1f
saaid323 <saaid2538@gmail.com>
2024-01-15 14:53:33 +0000

    added 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''measures the total execution time for wait_n(n, max_delay), and
 returns total_time / n'''
+import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
