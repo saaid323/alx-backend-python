4e7e49b4d05d54e7a32f2980f3302a943153d320
saaid323 <saaid2538@gmail.com>
2024-01-16 13:38:50 +0000

    added 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,10 +2,11 @@
 '''measure the total runtime and return it.'''
 import asyncio
 import time
+from typing import Generator
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> Generator[float, None, None]:
     '''function measure the total runtime and return it.'''
     start = time.time()
     await asyncio.gather(async_comprehension(), async_comprehension(),
