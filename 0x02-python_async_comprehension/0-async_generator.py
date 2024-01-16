1c4998cee8750f2644e2e3b97ecfe08aae612646
saaid323 <saaid2538@gmail.com>
2024-01-16 06:04:53 +0000

    added 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import List
 
 
-async def async_generator():
+async def async_generator() -> List[float]:
     '''function that yield a random number between 0 and 10'''
     for i in range(10):
         await asyncio.sleep(1)
