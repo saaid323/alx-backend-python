12ee2f9f310d1951be693f2a79249f2d9a94ba45
saaid323 <saaid2538@gmail.com>
2024-02-04 12:24:42 +0000

    added test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''unittesting client module'''
+from client import GithubOrgClient
+import unittest
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+from typing import Dict
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''class that inherits from unittest.TestCase'''
+
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch('client.get_json',)
+    def test_org(self, url: str, payload: Dict, mocked_get):
+        '''unittest for org'''
+        mocked_get.return_value = MagicMock(return_value=payload)
+        result = GithubOrgClient(url)
+        self.assertEqual(result.org(), payload)
+        mocked_get.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=url))
