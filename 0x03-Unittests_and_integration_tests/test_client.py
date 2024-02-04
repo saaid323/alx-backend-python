661bdd840022b3b4f79bf9a753222c7b9f6f2730
saaid323 <saaid2538@gmail.com>
2024-02-04 15:14:33 +0000

    added test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 '''unittesting client module'''
 from client import GithubOrgClient
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from typing import Dict
 
@@ -11,13 +11,10 @@ class TestGithubOrgClient(unittest.TestCase):
     '''class that inherits from unittest.TestCase'''
 
     @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"}),
-    @parameterized.expand([
         ("google", {'payload': False}),
         ("abc", {'payload': False}),
     ])
-    @patch('client.get_json',)
+    @patch('client.get_json')
     def test_org(self, url: str, payload: Dict, mocked_get):
         '''unittest for org'''
         mocked_get.return_value = MagicMock(return_value=payload)
@@ -25,3 +22,13 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result.org(), payload)
         mocked_get.assert_called_once_with(
             GithubOrgClient.ORG_URL.format(org=url))
+
+    def test_public_repos_url(self):
+        '''unittest for _public_repos_url'''
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+        as mock_last_transaction:
+            result = {'repos_url': 'result'}
+            mock_last_transaction.return_value = result
+            self.assertEqual(
+                GithubOrgClient('google')._public_repos_url,
+                'result')
